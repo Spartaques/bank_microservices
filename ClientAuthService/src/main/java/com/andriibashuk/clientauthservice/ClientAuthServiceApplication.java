0066c83cb354f93593294c3c5c5680ec69dbe4f3
Andrii Bashuk <96andlgrac@gmail.com>
2023-04-04 09:43:54 +0300

    ClientAuthService

---
+++ ClientAuthService/src/main/java/com/andriibashuk/clientauthservice/ClientAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.andriibashuk.clientauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAuthServiceApplication.class, args);
+	}
+
+}
---
+++ ClientAuthService/src/test/java/com/andriibashuk/clientauthservice/ClientAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriibashuk.clientauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
